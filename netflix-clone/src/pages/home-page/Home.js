504913febdb78e468424a8f43fef0b1c802c46bc
SalemAzezew <salemt1070@gmail.com>
2024-01-25 22:37:42 -0600

    banner created

--- netflix-clone/src/pages/home-page/Home.js
+++ netflix-clone/src/pages/home-page/Home.js
@@ -3,11 +3,13 @@ import Header from './Headerrr/Header.js';
 // import Headeeer from './Headerrr/Headeeer.css';
 // import Footerr from './Footer/Footerr.css';
 import Footer from './Footer/Footer.js';
+import Banner from './Banner/Banner.js';
 
 function Home() {
   return (
     <div>
         <Header />
+        <Banner />
         <Footer />
     </div>
   )
